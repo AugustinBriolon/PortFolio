b66bbc5b17bf26162e9ae51c1271ee862cd8aa85
Augustin <augustin.briolon@gmail.com>
2021-11-01 22:51:37 +0100

    projet finish

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,8 @@
-window.onload = () => {
-    window.addEventListener('scroll', () => {
-        let hauteur = document.documentElement.scrollHeight - window.innerHeight;
-        let position = window.scrollY;
-        let largeur = document.documentElement.clientWidth;
-
-        let barre = (position / hauteur) * largeur;
-
-        document.getElementById('barre').style.width = barre + "px";
-    });
-};
-
-
 var maintenant=new Date();
 var an = maintenant.getFullYear();
 an.toString();
 
 let copyright = document.querySelector('.copyright')
-copyright.textContent = ("©   - TOUS DROITS RÉSERVÉS");
+copyright.textContent = (an);
 
 
