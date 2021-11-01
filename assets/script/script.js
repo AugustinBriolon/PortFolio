6a4b7b4542231b0e584f1bc8d0d8efb485fde21f
Augustin <augustin.briolon@gmail.com>
2021-11-01 18:00:25 +0100

    css animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,21 @@
+window.onload = () => {
+    window.addEventListener('scroll', () => {
+        let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+        let position = window.scrollY;
+        let largeur = document.documentElement.clientWidth;
+
+        let barre = (position / hauteur) * largeur;
+
+        document.getElementById('barre').style.width = barre + "px";
+    });
+};
+
+
 var maintenant=new Date();
-var an=maintenant.getFullYear();
-document.write("© ",an," - TOUS DROITS RÉSERVÉS");
+var an = maintenant.getFullYear();
+an.toString();
+
+let copyright = document.querySelector('.copyright')
+copyright.textContent = ("©   - TOUS DROITS RÉSERVÉS");
+
 
