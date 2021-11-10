b4e17c8bbea3a4578889e0ee8423af6d2cf4d33f
Augustin <augustin.briolon@gmail.com>
2021-11-10 02:05:56 +0100

    :lipstick: add smooth anim

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,5 @@
+// Date Copyright
+
 var maintenant=new Date();
 var an = maintenant.getFullYear();
 an.toString();
@@ -6,3 +8,25 @@ let copyright = document.querySelector('.copyright')
 copyright.textContent = (an);
 
 
+// Animation Visible
+
+const ratio = .5
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('[class*="reveal"]').forEach(function (r) {
+    observer.observe(r)
+})
